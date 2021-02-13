119ecac8da1fffeb1ddc1f09b819c718e582ceac
Charlie Parsons <cdaltonparsons@gmail.com>
2021-02-13 13:09:02 -0700

    Fixed logic for storing measurements

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -43,21 +43,59 @@ export class HomePage {
     const loader = await this.loadingCtrl.create();
     await loader.present();
 
-    this.dataService.addMeasurementsShopper(this.shopperForm.value).then(
-      (result) => {
-        loader.dismiss();
-        this.router.navigate(['/tabs/explore']);
-      },
-      async (err) => {
-        loader.dismiss();
-        const alert = await this.alertCtrl.create({
-          header: 'Woops!',
-          message: err.message,
-          buttons: ['OK'],
-        });
+    if (this.segment == 'shopper') {
+      this.dataService.addMeasurementsShopper(this.measurementsForm.value).then(
+        async (result) => {
+          loader.dismiss();
+          const alert = await this.alertCtrl.create({
+            header: 'Thank You!',
+            message: `Our company is just launching and it looks like we don't have your exact fit right now.  Check back soon!`,
+            buttons: ['OK'],
+          });
+          alert.onDidDismiss().then((data) => {
+            this.router.navigate(['/tabs/explore']);
+          });
 
-        await alert.present();
-      }
-    );
+          await alert.present();
+        },
+        async (err) => {
+          loader.dismiss();
+          const alert = await this.alertCtrl.create({
+            header: 'Woops!',
+            message: err.message,
+            buttons: ['OK'],
+          });
+
+          await alert.present();
+        }
+      );
+    }
+    if (this.segment == 'model') {
+      this.dataService.addMeasurementsModel(this.measurementsForm.value).then(
+        async (result) => {
+          loader.dismiss();
+          const alert = await this.alertCtrl.create({
+            header: 'Thank You!',
+            message: `Now hit that + below to start creating!`,
+            buttons: ['OK'],
+          });
+          alert.onDidDismiss().then((data) => {
+            this.router.navigate(['/tabs/add-photo']);
+          });
+
+          await alert.present();
+        },
+        async (err) => {
+          loader.dismiss();
+          const alert = await this.alertCtrl.create({
+            header: 'Woops!',
+            message: err.message,
+            buttons: ['OK'],
+          });
+
+          await alert.present();
+        }
+      );
+    }
   }
 }
